3a61d42f1188785489a3e5fda99bf76a0df515b2
ryo-ma <saka_ro@yahoo.co.jp>
2020-06-12 17:53:50 +0900

    Support image to resize

--- scripts/audit_image.py
+++ scripts/audit_image.py
@@ -13,18 +13,22 @@ NODE_NAME = 'audit_image'
 def main():
     try:
         rospy.init_node(NODE_NAME)
+        # TODO: Replace all information to ros topics.
+        robot_id = 'robot_01'
         now = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
         location = '_'.join(['0.0', '0.0'])
         pos = '_'.join(['0.0', '0.0', '0.0', '0.0'])
         print("generate start")
         start = time.time()
-        watermark_text = '_'.join([now, location, pos])
+        watermark_text = ','.join([robot_id, now, location, pos])
 
         hashed_watermark_text = hashlib.sha224(watermark_text.encode()).hexdigest()
         print(hashed_watermark_text)
+        print(watermark_text)
         generator = WatermarkGenerator()
-        generator.generate(rospy.get_param('~')['watermark_image']['base_image_path'],
-                           hashed_watermark_text)
+        path = generator.generate(rospy.get_param('~')['watermark_image']['base_image_path'],
+                                  '{}\n{}'.format(hashed_watermark_text, watermark_text))
+        print(path)
         elapsed_time = time.time() - start
         print(str(elapsed_time) + "sec")
         print("generate finish")
--- src/ros_audit_image/watermark_generator.py
+++ src/ros_audit_image/watermark_generator.py
@@ -2,6 +2,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import rospy
 import os
+import uuid
 
 
 class WatermarkGenerator:
@@ -16,15 +17,23 @@ class WatermarkGenerator:
         text_color = self.__params['watermark_image']['text_color']
         self.color = (text_color['red'], text_color['green'], text_color['blue'])
         self.quality = self.__params['watermark_image']['quality']
+        self.output_width = self.__params['watermark_image']['output_width']
 
     def generate(self, image_path, text):
-        self.image = Image.open(image_path).convert('RGBA')
         self.image_path = image_path
+        self.image = Image.open(image_path).convert('RGBA')
+        self.image = self.scale_to_width(self.image, self.output_width)
         text_layer = Image.new('RGBA', self.image.size, (255, 255, 255, 0))
         draw = ImageDraw.Draw(text_layer)
         textw, texth = draw.textsize(text, font=self.font)
         draw.text(((self.image.width - textw) / 2, (self.image.height - texth) / 2),
                   text, font=self.font, fill=self.color + (self.opacity,))
         watermarked_image = Image.alpha_composite(self.image, text_layer)
-        watermarked_image.save(os.path.join(self.output_path, text),
+        path = '{}.png'.format(os.path.join(self.output_path, str(uuid.uuid4())))
+        watermarked_image.save(path,
                                'png', quality=self.quality, optimize=True)
+        return path
+
+    def scale_to_width(self, img, width):
+        height = round(img.height * width / img.width)
+        return img.resize((width, int(height)))
