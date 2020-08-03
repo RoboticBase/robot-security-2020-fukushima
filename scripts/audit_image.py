ea96b1960df79041e1bd62830d87c4aec1e0c02e
ryo-ma <saka_ro@yahoo.co.jp>
2020-08-03 11:23:42 +0900

    Change image processor from pillow to cv2

--- scripts/audit_image.py
+++ scripts/audit_image.py
@@ -5,33 +5,42 @@ import datetime
 import rospy
 import pytz
 import time
+from sensor_msgs.msg import Image as ImageMSG
 from ros_audit_image.watermark_generator import WatermarkGenerator
 
 NODE_NAME = 'audit_image'
 
 
+def callback(image_message):
+    # TODO: Replace all information to ros topics.
+    robot_id = 'robot_01'
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
+    location = '_'.join(['0.0', '0.0'])
+    pos = '_'.join(['0.0', '0.0', '0.0', '0.0'])
+    print("generate start")
+    start = time.time()
+    watermark_text = ','.join([robot_id, now, location, pos])
+
+    hashed_watermark_text = hashlib.sha224(watermark_text.encode()).hexdigest()
+    print(hashed_watermark_text)
+    print(watermark_text)
+    generator = WatermarkGenerator()
+    path = generator.generate(image_message,
+                              '{}\n{}'.format(hashed_watermark_text, watermark_text))
+
+
 def main():
     try:
         rospy.init_node(NODE_NAME)
-        # TODO: Replace all information to ros topics.
-        robot_id = 'robot_01'
-        now = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
-        location = '_'.join(['0.0', '0.0'])
-        pos = '_'.join(['0.0', '0.0', '0.0', '0.0'])
-        print("generate start")
-        start = time.time()
-        watermark_text = ','.join([robot_id, now, location, pos])
-
-        hashed_watermark_text = hashlib.sha224(watermark_text.encode()).hexdigest()
-        print(hashed_watermark_text)
-        print(watermark_text)
-        generator = WatermarkGenerator()
-        path = generator.generate(rospy.get_param('~')['watermark_image']['base_image_path'],
-                                  '{}\n{}'.format(hashed_watermark_text, watermark_text))
-        print(path)
-        elapsed_time = time.time() - start
-        print(str(elapsed_time) + "sec")
-        print("generate finish")
+        rospy.Subscriber('image_raw', ImageMSG, callback)
+        rospy.spin()
+        # generator = WatermarkGenerator()
+        # path = generator.generate(rospy.get_param('~')['watermark_image']['base_image_path'],
+        #                           '{}\n{}'.format(hashed_watermark_text, watermark_text))
+        # print(path)
+        # elapsed_time = time.time() - start
+        # print(str(elapsed_time) + "sec")
+        # print("generate finish")
     except rospy.ROSInterruptException:
         pass
 
--- src/ros_audit_image/watermark_generator.py
+++ src/ros_audit_image/watermark_generator.py
@@ -2,38 +2,35 @@
 from PIL import Image, ImageDraw, ImageFont
 import rospy
 import os
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 import uuid
 
 
 class WatermarkGenerator:
     def __init__(self):
+        self.bridge = CvBridge()
         self.__params = rospy.get_param('~')
         self.output_path = self.__params['watermark_image']['output_path']
-        font_size = self.__params['watermark_image']['font_size']
+        self.font_size = self.__params['watermark_image']['font_size']
         font_file_path = self.__params['watermark_image']['font_file_path']
-        self.font = ImageFont.truetype(font=font_file_path,
-                                       size=font_size)
-        self.opacity = self.__params['watermark_image']['text_opacity']
         text_color = self.__params['watermark_image']['text_color']
         self.color = (text_color['red'], text_color['green'], text_color['blue'])
-        self.quality = self.__params['watermark_image']['quality']
-        self.output_width = self.__params['watermark_image']['output_width']
+        self.text_position_x = self.__params['watermark_image']['text_position']['x']
+        self.text_position_y = self.__params['watermark_image']['text_position']['y']
+        self.newline_size = self.__params['watermark_image']['newline_size']
+        self.text_bold_size= self.__params['watermark_image']['text_bold_size']
 
-    def generate(self, image_path, text):
-        self.image_path = image_path
-        self.image = Image.open(image_path).convert('RGBA')
-        self.image = self.scale_to_width(self.image, self.output_width)
-        text_layer = Image.new('RGBA', self.image.size, (255, 255, 255, 0))
-        draw = ImageDraw.Draw(text_layer)
-        textw, texth = draw.textsize(text, font=self.font)
-        draw.text(((self.image.width - textw) / 2, (self.image.height - texth) / 2),
-                  text, font=self.font, fill=self.color + (self.opacity,))
-        watermarked_image = Image.alpha_composite(self.image, text_layer)
+    def generate(self, image_message, text):
+        cv_image = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='bgr8')
+        self.put_text(cv_image, text)
         path = '{}.png'.format(os.path.join(self.output_path, str(uuid.uuid4())))
-        watermarked_image.save(path,
-                               'png', quality=self.quality, optimize=True)
+        cv2.imwrite(path, cv_image)
         return path
 
-    def scale_to_width(self, img, width):
-        height = round(img.height * width / img.width)
-        return img.resize((width, int(height)))
+    def put_text(self, image, text):
+        y0, dy = self.text_position_y, self.newline_size
+        for i, line in enumerate(text.split('\n')):
+            y = y0 + i*dy
+            cv2.putText(image, line, (self.text_position_x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        self.font_size, self.color, self.text_bold_size)
