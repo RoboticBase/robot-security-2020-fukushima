466561473fd5bfd97a875d191a26a5681958fed9
ryo-ma <saka_ro@yahoo.co.jp>
2020-11-02 03:12:18 +0900

    Fix variable name

--- src/stuck_checker.ts
+++ src/stuck_checker.ts
@@ -133,15 +133,14 @@ export class StuckChecker {
   // Calculate the earth as a true sphere.
   // Correctly, it should be calculated considering GRS80
   calculateDistance(point1: Point, point2: Point) {
-    const EARTH_DISTANCE = 6371000;
-    const RADIUS = 180;
+    const EARTH_RADIUS = 6371000;
 
-    const distanceMeters = EARTH_DISTANCE * Math.acos(
-      Math.cos(point1.latitude / RADIUS * Math.PI) *
-          Math.cos((point2.longitude - point1.longitude) / RADIUS * Math.PI) *
-          Math.cos(point2.latitude / RADIUS * Math.PI) +
-        Math.sin(point1.latitude / RADIUS * Math.PI) *
-          Math.sin(point2.latitude / RADIUS * Math.PI),
+    const distanceMeters = EARTH_RADIUS * Math.acos(
+      Math.cos(point1.latitude / 180 * Math.PI) *
+          Math.cos((point2.longitude - point1.longitude) / 180 * Math.PI) *
+          Math.cos(point2.latitude / 180 * Math.PI) +
+        Math.sin(point1.latitude / 180 * Math.PI) *
+          Math.sin(point2.latitude / 180 * Math.PI),
     );
     return distanceMeters;
   }
