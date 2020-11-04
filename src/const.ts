44c0bd1aa08e264305734113b2c6b34de44b528a
ryo-ma <saka_ro@yahoo.co.jp>
2020-11-04 12:49:21 +0900

    Support to load loop sleep second parameter from environment

--- src/const.ts
+++ src/const.ts
@@ -2,7 +2,7 @@ import { getEnvNumberValue } from "./utils.ts";
 
 export const DEFAULT_ALLOWABLE_COUNT = getEnvNumberValue("ALLOWABLE_COUNT", 5);
 export const DEFAULT_RANGE_METERS = getEnvNumberValue("RANGE_METERS", 1);
-export const DEFAULT_LOOP_SLEEP_SECOND = 5;
+export const DEFAULT_LOOP_SLEEP_SECOND = getEnvNumberValue("LOOP_SLEEP_SECOND", 5);
 export const DEFAULT_ORION_ENDPOINT = Deno.env.get("ORION_ENDPOINT") ||
   "http://orion:1026";
 export const DEFAULT_ENTITY_ID = Deno.env.get("ENTITY_ID") || "robot01";
