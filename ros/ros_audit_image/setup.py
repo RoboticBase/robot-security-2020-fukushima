a7dd41257549653f0ec33c8845852932aa47c5e9
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2021-03-08 17:13:30 +0900

    added ros/ros_audit_image

---
+++ ros/ros_audit_image/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=['ros_audit_image'],
+    package_dir={'': 'src'},
+    install_requires=[],
+)
+
+setup(**setup_args)
